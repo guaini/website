ef48c04ae10433738647bd9fe7dcd0c0bb5f6c4f
guaini <992937344@qq.com>
2023-06-30 20:22:37 +0800

    修改url

--- homework/urls.py
+++ homework/urls.py
@@ -13,10 +13,27 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
+from django.views.generic.base import TemplateView
 from django.urls import path
+from django.conf.urls import include, url
+from django.conf.urls.static import static
+
+from backend import search, views
 
 urlpatterns = [
+    path('api/', include('backend.urls')),
     path('admin/', admin.site.urls),
+    path('search', search.search),
+    url(r"^$", views.index),
+    url('index', views.index, name="index"),
+    url('detail', views.detail, name="detail"),
+    url('login', views.to_login, name="login"),
+    url('set', views.to_set, name="set"),
+    url('star', views.to_star, name="star"),
+    url('upload', views.to_upload, name="upload"),
 ]
 
+urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns = urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
